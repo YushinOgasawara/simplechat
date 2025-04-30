0d439a38e3bc1eb078f44684f55742d7ea767877
YushinOgasawara <168957325+YushinOgasawara@users.noreply.github.com>
2025-04-30 15:41:13 +0900

    [fix] FastAPIのURLを新しいエンドポイントに更新

--- lambda/index.py
+++ lambda/index.py
@@ -7,7 +7,7 @@ from botocore.exceptions import ClientError
 import urllib.request
 import urllib.error
 
-FASTAPI_URL = "https://14ce-34-138-177-216.ngrok-free.app"
+FASTAPI_URL = "https://8906-34-125-170-151.ngrok-free.app"
 
 # Lambda コンテキストからリージョンを抽出する関数
 def extract_region_from_arn(arn):
